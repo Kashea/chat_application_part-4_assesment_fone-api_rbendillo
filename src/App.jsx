2ae36f76ed530358b0135af4eb533b63ff52f372
Kashea <camrynchelsea01@gmail.com>
2023-06-29 03:19:03 +0800

    created new component LoginForm, updated App.JS, updated LoginForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
 import { ChatEngine } from 'react-chat-engine';
 
 import ChatFeed from './components/ChatFeed';
-
+import LoginForm from './components/LoginForm';
 import './App.css';
 
+const projectID = '1b7801d6-8a66-4be4-a442-89219d833dfc';
+
 const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
   return (
     <ChatEngine
-        height ="100vh"
-        projectID="
-        081d0466-82be-45c2-afc4-04836cf67e16"
-        userName="Rossrival Bendillo"
-        userSecret="M@halkita01"
-        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
     />
   );
-}
+};
+
+// infinite scroll, logout, more customizations...
 
 export default App;
-
