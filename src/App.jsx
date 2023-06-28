619d54cbfcc5d0cecffeec34dbd29666d09baec2
Kashea <camrynchelsea01@gmail.com>
2023-06-28 19:00:17 +0800

    chatengine.io integration created, updated app.jsx, create test users, test demo chat, status working,updating css, created a new component ChatFeed

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,21 @@
 import { ChatEngine } from 'react-chat-engine';
 
+import ChatFeed from './components/ChatFeed';
+
 import './App.css';
+
+const App = () => {
+  return (
+    <ChatEngine
+        height ="100vh"
+        projectID="
+        081d0466-82be-45c2-afc4-04836cf67e16"
+        userName="Rossrival Bendillo"
+        userSecret="M@halkita01"
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
+    />
+  );
+}
+
+export default App;
+
